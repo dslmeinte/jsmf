08b0cd5655dc63875b52de9f38f897ebac2b7d28
Meinte Boersma <meinte.boersma@gmail.com>
2012-10-24 12:01:53 +0200

    added documentation

--- nl.dslmeinte.jsmf/src/oo-util.js
+++ nl.dslmeinte.jsmf/src/oo-util.js
@@ -10,8 +10,17 @@ oo.util = new (function() {
 
 	/**
 	 * Sets up inheritance on the given base and sub types, including constructors.
-	 * 
+	 * <p>
 	 * See: http://stackoverflow.com/questions/4152931/javascript-inheritance-call-super-constructor-or-use-prototype-chain
+	 * <p>
+	 * Usage pattern:
+	 * <pre>
+	 * function SuperClass(initData) {}
+	 * function SubClass(initData) {
+	 * 	SuperClass.call(this, [ initData ]);
+	 * }
+	 * oo.util.extend(SuperClass, SubClass);
+	 * </pre>
 	 */
 	this.extend = function(base, sub) {
 		function delegateConstructor() { /* do nothing */ }
